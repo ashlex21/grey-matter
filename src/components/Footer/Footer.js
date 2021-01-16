29f2bff9a8e634e4bfc0005f35c88282a8733403
ashlex21 <aashishsah008@gmail.com>
2021-01-15 17:45:05 -0800

    addition of footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Footer = () => {
+    return (<div>
+        <footer>
+        <p> Designed by Ashlex | ©21-Technologies 2021</p>
+            <p>This website and its content is copyright of 21 Technologies</p>
+        </footer>
+    </div>)
+}
